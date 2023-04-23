fcea69d74a42744adf446f95c852f8ea53e75d07
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-23 17:48:53 +0300

    Add 'created_at' field to User model

--- weeb/users/models.py
+++ weeb/users/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=32, unique=True, editable=False, blank=False)
+    created_at = models.DateTimeField('Registration date', auto_now_add=True)
 
 
 class UserProfile(models.Model):
