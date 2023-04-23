bac5e7181bed0910de37f65be94ed10080cdf618
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-23 19:17:20 +0300

    Email field to User & changes in admin

--- weeb/users/admin.py
+++ weeb/users/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import User, UserProfile
 
 
-admin.site.register(User)
-admin.site.register(UserProfile)
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(UserProfile)
+class UserProfileModelAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
