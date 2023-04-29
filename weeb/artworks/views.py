32ccb76fefa117778ecfb3a795a9edb4a53dec0a
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-04-29 10:34:40 +0300

    Base home page

--- weeb/artworks/views.py
+++ weeb/artworks/views.py
@@ -1,6 +1,19 @@
+from django.db.models import Count
 from django.shortcuts import render
+
+from .models import Artwork, Tag
 
 
 def home_page(request):
-    context = {}
-    return render(request, 'main.html', context)
+    artworks = Artwork.objects.all()
+    all_tags_count = Tag.objects.all().count()
+    popular_tags = Tag.objects.annotate(num_artworks=Count('artwork_tags')).order_by(
+        '-num_artworks'
+    )[:10]
+    context = {
+        'artworks': artworks,
+        'all_tags_count': all_tags_count,
+        'popular_tags': popular_tags,
+    }
+
+    return render(request, 'home.html', context)
