72495de39c8a83b51559a3c678cb34e2fb1e005c
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-05-03 17:07:51 +0300

    Update urls

--- weeb/artworks/urls.py
+++ weeb/artworks/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.home_page, name='home'),
 
     path('artwork/<uuid:pk>/', views.artwork_page, name='artwork'),
-    path('artwork-create/', views.create_artwork, name='artwork-create'),
-    path('artwork-edit/<uuid:pk>', views.edit_artwork, name='artwork-edit'),
-    path('artwork-delete/<uuid:pk>/', views.delete_artwork, name='artwork-delete'),
+    path('artwork/create/', views.create_artwork, name='artwork-create'),
+    path('artwork/edit/<uuid:pk>/', views.edit_artwork, name='artwork-edit'),
+    path('artwork/delete/<uuid:pk>/', views.delete_artwork, name='artwork-delete'),
 
     path('tags/', views.tags_page, name='tags'),
     path(
