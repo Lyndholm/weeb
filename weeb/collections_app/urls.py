8ff9aac74160c84267a88a37edb8ace56e500551
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-05-07 14:38:18 +0300

    Create collection functionality

--- weeb/collections_app/urls.py
+++ weeb/collections_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    
+    path('collections/create/', views.create_collection, name='collection-create'),
 ]
--- weeb/collections_app/views.py
+++ weeb/collections_app/views.py
@@ -1,1 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import CollectionCreateForm
+
+
+@login_required(login_url='login')
+def create_collection(request):
+    form = CollectionCreateForm()
+
+    if request.method == 'POST':
+        form = CollectionCreateForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save(request=request)
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'collection_create.html', context)
