8b2bb8feb1dc379e59eb0e328fce45c2bd8e8dc5
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-05-07 16:32:14 +0300

    Collections page

--- weeb/collections_app/urls.py
+++ weeb/collections_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('collections/', views.collections_home_page, name='collections'),
     path('collections/create/', views.create_collection, name='collection-create'),
 ]
--- weeb/collections_app/views.py
+++ weeb/collections_app/views.py
@@ -1,7 +1,17 @@
+from artworks.views import paginate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 from .forms import CollectionCreateForm
+from .models import Collection
+
+
+def collections_home_page(request):
+    collections = Collection.objects.all()
+    collections = paginate(request, collections, 10)
+
+    context = {'collections': collections}
+    return render(request, 'collections.html', context)
 
 
 @login_required(login_url='login')
@@ -13,7 +23,7 @@ def create_collection(request):
 
         if form.is_valid():
             form.save(request=request)
-            return redirect('home')
+            return redirect('collections')
 
     context = {'form': form}
     return render(request, 'collection_create.html', context)
