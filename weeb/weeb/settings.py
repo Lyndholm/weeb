3e4ebaf2bfc90ff9475a23f4733f3a2f6d5ca147
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-05-09 11:48:33 +0300

    Update static and media

--- weeb/weeb/settings.py
+++ weeb/weeb/settings.py
@@ -108,12 +108,12 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'images/'
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / STATIC_URL,
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
