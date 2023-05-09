eb83a01d3e38098619f123a9eba7ecdcafc3ecb0
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-05-09 19:46:44 +0300

    Base docker setup

---
+++ weeb/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Waiting for PostgreSQL..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+        sleep 1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate
+
+exec "$@"
