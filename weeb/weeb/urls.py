fce5cabef8e9deaa82ec2396482d36e6cb6f06b4
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2023-05-10 23:01:19 +0300

    Change admin url

--- weeb/weeb/urls.py
+++ weeb/weeb/urls.py
@@ -1,14 +1,17 @@
+import os
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+admin_route = os.getenv('DJANGO_ADMIN_URL', 'admin/')
+
 urlpatterns = [
     path('', include('users.urls')),
     path('', include('artworks.urls')),
     path('', include('collections_app.urls')),
-
-    path('admin/', admin.site.urls),
+    path(admin_route, admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
